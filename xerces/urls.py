2dd8a4628feed0bc686e44aebf9cb543d8ba8f84
Gwen Reynolds <gwen.reynolds.94@gmail.com>
2024-10-23 16:13:57 -0400

    .

--- xerces/urls.py
+++ xerces/urls.py
@@ -33,3 +33,10 @@ urlpatterns = [
         name='site_path'
     ),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
