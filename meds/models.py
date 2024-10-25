34488d4d3a775b1a3678251c07cbd29217972a72
Gwen Reynolds <gwen.reynolds.94@gmail.com>
2024-10-25 17:41:33 -0400

    .

--- meds/models.py
+++ meds/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Medication(models.Model):
+    name = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Prescription(models.Model):
+    MEASUREMENT_CHOICES = {
+        'mg': 'Milligrams',
+        'gm': 'Grams',
+        'ml': 'Milliliters',
+        'cp': 'Cups',
+    }
+    medication = models.ForeignKey(Medication, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    measurement = models.CharField(max_length=2, choices=MEASUREMENT_CHOICES)
+
--- meds/views.py
+++ meds/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.views.generic import TemplateView
+from .models import Medication
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "meds/index.html"
 
 # Create your views here.
+class UpdateMedicationList(LoginRequiredMixin, View):
+    def post(self, request):
+        from bs4 import BeautifulSoup
+        import requests
+        
+        az = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        names = []
+        for alpha in az:
+            r = requests.get(f"http://rxlist.com/drugs/alpha_{alpha}.htm")
+            soup = BeautifulSoup(r.text, features="html.parser")
+            div = soup.find_all("div", class_="AZ_results")[0]
+            for li in div.find_all("li"):
+                names.append(li.text)
+        
+        Medication.objects.all().delete()
+        for name in names:
+            Medication(name=name).save()
+        
+        return redirect("meds/index.html")
