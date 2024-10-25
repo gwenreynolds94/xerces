32b47e699211edd80ca96d8416de9a7a91fe0b59
gwenreynolds94 <gwen.reynolds.94@gmail.com>
2024-10-25 22:42:00 +0000

    .

--- meds/views.py
+++ meds/views.py
@@ -9,10 +9,12 @@ class IndexView(LoginRequiredMixin, TemplateView):
 
 # Create your views here.
 class UpdateMedicationList(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, "meds/index.html")
     def post(self, request):
         from bs4 import BeautifulSoup
         import requests
-        
+
         az = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         names = []
         for alpha in az:
@@ -21,9 +23,9 @@ class UpdateMedicationList(LoginRequiredMixin, View):
             div = soup.find_all("div", class_="AZ_results")[0]
             for li in div.find_all("li"):
                 names.append(li.text)
-        
+
         Medication.objects.all().delete()
         for name in names:
             Medication(name=name).save()
-        
-        return redirect("meds/index.html")
+
+        return redirect(request.path_info)
